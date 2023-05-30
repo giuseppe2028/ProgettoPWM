0f34fbadc2351ca6e0c54ce6f27cab3073c14e03
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-05-30 11:37:57 +0200

    cambiato colore alle icone

--- app/src/main/java/com/example/progettopwm/SchermataHome/SchermataHome.kt
+++ app/src/main/java/com/example/progettopwm/SchermataHome/SchermataHome.kt
@@ -6,7 +6,6 @@ import com.example.progettopwm.R
 
 class SchermataHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schermata_home)
     }
