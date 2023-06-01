c2e588f9a2cecf5ac7ef30e2ef7ab766616aaf2c
gabri <gabriele.raffaele87@gmail.com>
2023-06-01 11:41:03 +0200

    schermata wallet

---
+++ app/src/main/java/com/example/progettopwm/SchermataHome/SchermataWallet.kt
@@ -0,0 +1,12 @@
+package com.example.progettopwm.SchermataHome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.progettopwm.R
+
+class SchermataWallet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schermata_wallet)
+    }
+}
