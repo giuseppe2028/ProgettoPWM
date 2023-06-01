9b39669e008a06492e7577acb7eb6e53b8522c3c
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-01 17:53:08 +0200

    Implementazione Notifiche

--- app/src/main/java/com/example/progettopwm/SchermataHome/SchermataHome.kt
+++ app/src/main/java/com/example/progettopwm/SchermataHome/SchermataHome.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.progettopwm.R
 
 class SchermataHome : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schermata_home)
--- app/src/main/java/com/example/progettopwm/SchermataRegistrazione/SchermataRegistrazione.kt
+++ app/src/main/java/com/example/progettopwm/SchermataRegistrazione/SchermataRegistrazione.kt
@@ -1,16 +1,21 @@
 package com.example.progettopwm.SchermataRegistrazione
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.InputType
 import android.text.TextWatcher
+import android.util.Log
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.databinding.DataBindingUtil
 import com.example.progettopwm.R
 import com.example.progettopwm.databinding.ActivitySchermataRegistrazioneBinding
@@ -18,7 +23,6 @@ import com.example.progettopwm.databinding.ActivitySchermataRegistrazioneBinding
 class SchermataRegistrazione : AppCompatActivity() {
     private lateinit var binding: ActivitySchermataRegistrazioneBinding
     private var isPasswordVisible = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySchermataRegistrazioneBinding.inflate(layoutInflater)
@@ -29,6 +33,7 @@ class SchermataRegistrazione : AppCompatActivity() {
             transaction.add(R.id.fragmentContainerView, FragmentRegistrazione())
             transaction.commit()
         }
+
 
     }
 
