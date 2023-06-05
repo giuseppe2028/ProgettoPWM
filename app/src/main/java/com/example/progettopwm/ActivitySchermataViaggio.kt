ad77c6b004b5c30a9795fe27570f57446ab0ab1a
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-05 09:10:39 +0200

    Sistemata la schermata

--- app/src/main/java/com/example/progettopwm/ActivitySchermataViaggio.kt
+++ app/src/main/java/com/example/progettopwm/ActivitySchermataViaggio.kt
@@ -2,6 +2,7 @@ package com.example.progettopwm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.graphics.drawable.toDrawable
 import com.example.progettopwm.databinding.SchermataViaggioBinding
 
@@ -12,11 +13,16 @@ class ActivitySchermataViaggio : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = SchermataViaggioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var stato:Boolean = false
         binding.like.setOnClickListener {
-            if(binding.like.isEnabled){
+            Log.i("ciao","prova1")
+            if(stato){
                 binding.like.setImageDrawable(getDrawable(R.drawable.heartbutton))
-            }else{
+                stato = false
+            }else if(!stato){
+                Log.i("ciao","prova3")
                 binding.like.setImageDrawable(getDrawable(R.drawable.heart_3))
+                stato = true
             }
         }
     }
