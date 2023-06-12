77e35286f6146099b45ca3c8a74765e962939c31
gabri <gabriele.raffaele87@gmail.com>
2023-06-12 18:30:19 +0200

    ho introdott la classe idPersona che è un singleton

---
+++ app/src/main/java/com/example/progettopwm/idPersona.kt
@@ -0,0 +1,13 @@
+package com.example.progettopwm
+
+object idPersona {
+    private var id: Int = 0
+
+    fun setId(value: Int) {
+        id = value
+    }
+
+    fun getId(): Int {
+        return id
+    }
+}
