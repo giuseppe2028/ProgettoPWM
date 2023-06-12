c2b8d201e5e9321d1695e02e1cfb0b38cf860f1e
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-12 23:34:11 +0200

    CustomAdaper recensioni implementato

--- app/src/main/java/com/example/progettopwm/Recensioni/RecyclerView/CustomAdapterRecensioni.kt
+++ app/src/main/java/com/example/progettopwm/Recensioni/RecyclerView/CustomAdapterRecensioni.kt
@@ -1,21 +1,41 @@
 package com.example.progettopwm.Recensioni.RecyclerView
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.example.progettopwm.SchermataHome.RecycleView.CustomAdapter
+import com.example.progettopwm.databinding.CardLocalitaBinding
+import com.example.progettopwm.databinding.CardRecensioneBinding
 
-class CustomAdapterRecensioni(val lista:List<ItemViewModelRecensioni>):RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
+class CustomAdapterRecensioni(val lista:List<ItemViewModelRecensioni>):RecyclerView.Adapter<CustomAdapterRecensioni.ViewHolder>() {
+    class ViewHolder(binding:CardRecensioneBinding):RecyclerView.ViewHolder(binding.root){
+        val titolo = binding.titolo
+        val rating  =binding.ratingBar4
+        val testo = binding.testo
+        val immagine = binding.imageProfile
+        val utente  = binding.utente
 
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = CardRecensioneBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return lista.size
     }
 
-    override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
+        val item = lista[position]
+        holder.testo.text = item.testo
+        holder.utente.text = item.utente
+        holder.rating.rating = item.rating.toFloat()
+        //TODO(aggiungere immagine)
+        holder.titolo.text = item.titolo
+
     }
 
 }
