ccaf58e5f33eae66ba53e654bbeaaf246cb4c4d0
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-13 11:51:20 +0200

    Filtro Implementato

--- app/src/main/java/com/example/progettopwm/Filter.kt
+++ app/src/main/java/com/example/progettopwm/Filter.kt
@@ -3,17 +3,37 @@ package com.example.progettopwm
 import android.app.Activity
 import android.app.Dialog
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Window
+import android.widget.Button
+import android.widget.Spinner
 import com.example.progettopwm.databinding.FiltriBinding
 
 class ViewDialog {
+
     private lateinit var binding:FiltriBinding
-    fun showResetPasswordDialog(activity: Activity?) {
+    //TODO(aggiungere la data, e il prezzo)
+    fun showDialog(activity: Activity?,callback:(String,String)->Unit) {
         val dialog = Dialog(activity!!)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(false)
         dialog.setContentView(R.layout.filtri)
-        binding = FiltriBinding.inflate(activity.layoutInflater)
+        val dialogBtn_remove = dialog.findViewById<Button>(R.id.bottoneConferma)
+
+        dialogBtn_remove.setOnClickListener {
+            //prendo i dati e li passo alla classe di sopra:
+            val regione = dialog.findViewById<Spinner>(R.id.regione)
+            val numPersoneView  = dialog.findViewById<Spinner>(R.id.numPersone)
+            val destianazione = dialog.findViewById<Spinner>(R.id.regione).selectedItem.toString()
+            val numPersone = numPersoneView.selectedItem.toString()
+            val valore = regione.selectedItem.toString()
+
+            Log.i("ciao","$destianazione")
+            callback(destianazione,numPersone)
+            dialog.dismiss()
+
+        }
+
 
 
         //val dialogBtn_remove = dialog.findViewById<TextView>(R.id.txtClose)
@@ -22,5 +42,6 @@ class ViewDialog {
         //activity!!.finish()
 
         dialog.show()
+
     }
 }
