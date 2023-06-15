f45d4d9de3ffe2f33ab48b42ae45690befe182b6
Giuseppe Barone <78317934+giuseppe2028@users.noreply.github.com>
2023-06-15 16:06:59 +0200

    Aggiunti controlli

--- app/src/main/java/com/example/progettopwm/Recensioni/RecensioneScritta.kt
+++ app/src/main/java/com/example/progettopwm/Recensioni/RecensioneScritta.kt
@@ -2,6 +2,8 @@ package com.example.progettopwm.Recensioni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.progettopwm.R
 import com.example.progettopwm.databinding.ActivityScriviRecensioneBinding
 
@@ -12,6 +14,28 @@ class RecensioneScritta : AppCompatActivity() {
         binding = ActivityScriviRecensioneBinding.inflate(layoutInflater)
         setContentView(binding.root)
        val idViaggio = intent.getIntExtra("idViaggio",0)
+        clickBottone()
+    }
 
+    private fun clickBottone() {
+        val titolo = binding.insersciTitolo.text
+        val testo = binding.inserisciTesto.text
+        val rating = binding.ratingScrittura.rating
+        Log.i("debuggg", "$titolo,$testo,$rating")
+        //controllo i campi:
+        binding.confermaButton.setOnClickListener {
+            when{
+                titolo.toString() == "" -> Toast.makeText(this,R.string.InserisciTitolo,Toast.LENGTH_SHORT).show()
+                testo.toString() == "" ->Toast.makeText(this,R.string.InserisciTesto,Toast.LENGTH_SHORT).show()
+                rating.toDouble() == 0.0 ->Toast.makeText(this,R.string.InserisciValuta,Toast.LENGTH_SHORT).show()
+                else -> {
+                    registraRisposta()
+                }
+            }
+        }
+    }
+
+    private fun registraRisposta() {
+        val query = "insert into Recensione values()"
     }
 }
